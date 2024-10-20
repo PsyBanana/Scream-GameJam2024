83489874f134654ebbe7744ab96bba237961a19b
PsyBanana <83030305+PsyBanana@users.noreply.github.com>
2024-10-20 14:05:21 +0300

    2D Animations
    Some small adjustments to moving scripts + animations

--- Assets/2D/Scripts/Player2D/PlayerMovement2D.cs
+++ Assets/2D/Scripts/Player2D/PlayerMovement2D.cs
@@ -2,18 +2,19 @@ using UnityEngine;
 
 public class PlayerMovement2D : MonoBehaviour
 {
-
     public float speed = 10f;
 
     private Rigidbody rb;
+
+    public Animator animator;
+
+    Vector2 movement;
 
     public bool canMove = false;
 
     void Start()
     {
-
         rb = GetComponent<Rigidbody>();
-
     }
 
     void Update()
@@ -24,20 +25,55 @@ public class PlayerMovement2D : MonoBehaviour
         }
         else
         {
-            rb.velocity = Vector2.zero;
+            rb.velocity = Vector2.zero; 
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
+        if (canMove)
+        {
+            
+            Vector3 moveVector = new Vector3(movement.x, movement.y, 0);
+
+           
+            rb.MovePosition(rb.position + moveVector * speed * Time.fixedDeltaTime);
         }
     }
 
     private void Move()
     {
-        float moveX = Input.GetAxisRaw("Horizontal"); // Use GetAxisRaw for immediate response
+        float moveX = Input.GetAxisRaw("Horizontal"); // Immediate response
         float moveY = Input.GetAxisRaw("Vertical");
 
-        // Create the movement vector
-        Vector2 moveDirection = new Vector2(moveX, moveY).normalized;
+       
+        movement = new Vector2(moveX, moveY).normalized;
 
-        // Set the velocity directly based on input
-        rb.velocity = moveDirection * speed;
+        animator.SetFloat("Horizontal", moveX);
+        animator.SetFloat("Vertical", moveY);
+        animator.SetFloat("Speed", movement.sqrMagnitude); 
 
+
+        // Determine last direction based on movement
+        if (movement != Vector2.zero)
+        {
+            if (moveY > 0)
+            {
+                animator.SetInteger("LastDirection", 0); // Up
+            }
+            else if (moveY < 0)
+            {
+                animator.SetInteger("LastDirection", 1); // Down
+            }
+            else if (moveX > 0)
+            {
+                animator.SetInteger("LastDirection", 2); // Right
+            }
+            else if (moveX < 0)
+            {
+                animator.SetInteger("LastDirection", 3); // Left
+            }
+        }
     }
 }
